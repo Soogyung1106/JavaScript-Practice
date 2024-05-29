e9c3965cf6571b230fc9fda25f6a809b2f40fe24
Soogyung Jung <sally1106@naver.com>
2024-05-29 11:08:02 +0900

    자료형 실습

---
+++ section01/chapter05.js
@@ -0,0 +1,42 @@
+//1. Number Type
+
+let num1 = 28;
+let num2 = 1.5;
+let num3 = -20;
+let num4 = 0n;
+
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+
+let inf = Infinity
+let mInt = -Infinity
+
+console.log(Infinity) //출력하면 Infinity
+let nan = NaN
+
+
+//2. String Type
+let myName = "동수"
+let myLocation = "시크릿"
+let introduce = myName + myLocation
+
+let introduceText = '${myName}은 ${myLocation}에 거주합니다';
+console.log(introduceText)
+
+
+//3. Boolean Type 
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Null Type (아무것도 없다)
+let empty = null
+
+//5. Undefined Type
+let none;  //초기화 되지 않았을 때
+console.log(none);  //undefined를 출력한다.
+
